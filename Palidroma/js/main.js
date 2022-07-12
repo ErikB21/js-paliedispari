ae905581fef41e4f2bff395156dc54e7fea81bee
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-12 23:27:16 +0200

    finit pari e dispari

---
+++ Palidroma/js/main.js
@@ -0,0 +1,30 @@
+//richiedere l'immissione di una parola
+
+const inserireParola = prompt('Prego, inserire la parola misteriosa');
+console.log(inserireParola);
+
+//creare variabili che definiscano la splittazione della parola immessa
+
+let text = inserireParola.split('');
+console.log(text);
+
+let alContrario = text.reverse();
+console.log(alContrario);
+
+let valid = false;
+//creare una funzione con al suo interno un ciclo, cosi facendo scopriremo se la parola immessa è palindroma
+
+function ifPalindromo(poli){
+    for(let i = 0; i < text.length; i++){
+        if(text[0] === poli[0]){
+            valid = true;
+            return "E' un palindromo";
+        }
+    }if(text !== poli){
+        valid = false;
+        return "Non è un palindromo";
+    }
+}
+//verifico il tutto attraverso la console.
+const verifica = ifPalindromo(inserireParola);
+console.log(verifica);
