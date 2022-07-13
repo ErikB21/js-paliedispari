33a8b9c1f1147d9b4c7bc293fc0ae5cbeb16efa6
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-13 10:17:18 +0200

    add soluzione prof

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -1,5 +1,6 @@
 //richiedere l'immissione di una parola
 
+/*
 const inserireParola = prompt('Prego, inserire la parola misteriosa');
 console.log(inserireParola);
 
@@ -28,3 +29,35 @@ function ifPalindromo(poli){
 //verifico il tutto attraverso la console.
 const verifica = ifPalindromo(inserireParola);
 console.log(verifica);
+*/
+
+
+let inserireParola = prompt('Prego, inserire la parola misteriosa');
+console.log(inserireParola);
+
+inserireParola = inserireParola.replaceAll(' ', '');
+inserireParola = inserireParola.toLowerCase();
+
+const parolaGirata = giraParola(inserireParola);
+console.log(inserireParola, parolaGirata);
+
+if( inserireParola == parolaGirata){
+    alert("E' un palindromo")
+}else{
+    alert("Non è palindromo")
+}
+
+
+
+function giraParola(parola){
+    //const parolaGirata = parola.split('').reverse().join('');
+    let parolaGirata = '';
+
+    for(let i = parola.length - 1; i >= 0; i--){
+        parolaGirata += parola[i];
+    }
+    return parolaGirata;
+}
+
+
+
